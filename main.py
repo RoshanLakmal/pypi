701184e799d309a929f1e3f0cf87a04eaa2a8c8d
Roshan <roshanrox911@gmail.com>
2023-08-26 21:09:47 +1000

    starter

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from prettytable import PrettyTable
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+table = PrettyTable()
+table.field_names = ["Pokemon Name", "Type"]
+table.add_rows(
+    [
+        ["Pikachu", "Electric"],
+        ["Squirtle", "Water"],
+        ["Charmander", "Fire"],
+    ]
+)
+table.align = "l"
+print(table)
